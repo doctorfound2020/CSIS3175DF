53c0073cea5a4ff5489dce8928d8048691158390
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-03-17 05:23:30 -0700

    Turned MainActivity.java into splash page. After 5 seconds, goes to LoginActivity.java.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Doctor Found Welcome");
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task,5000);
     }
 }
