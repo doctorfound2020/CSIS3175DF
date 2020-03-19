6ca3867e20308915009a8a5ecd3ed1d359a19b6d
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-03-18 21:56:28 -0700

    Completed the layout for the SignUpActivity.java, and programmed some buttons for both the SignUpActivity.java and LoginActivity.java classes. Several Strings were altered.

--- app/src/main/java/com/example/test/LoginActivity.java
+++ app/src/main/java/com/example/test/LoginActivity.java
@@ -3,8 +3,10 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,6 +17,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         setTitle("Doctor Found Login");
+
+        Button btnToSignUp = findViewById(R.id.btnSignUpSI);
+
+        btnToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/test/SignUpActivity.java
+++ app/src/main/java/com/example/test/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        setTitle("Doctor Found Sign Up");
+
+        Button btnToSignIn = findViewById(R.id.btnSignInSU);
+
+        btnToSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
+
+
     }
 }
