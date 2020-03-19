98b8061c01ad251856cb9ff0d9bd38dfc7153fcb
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-03-19 01:43:47 -0700

    Added empty home pages for all 4 users(java and xml file), as well as EditAccountActivity.java, which is linked to activity_sign_up.xml.

---
+++ app/src/main/java/com/example/test/CashierHomePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CashierHomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cashier_home_page);
+    }
+}
