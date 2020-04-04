2c3416bd4b8f7b092534bce45bdf0f3f9602c472
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-04-04 03:01:07 -0700

    Added about half of the database code for login/signup, as well as fixed error: cant have spaced for column names for tables(removed spaces)

--- app/src/main/java/com/example/test/DFhelper.java
+++ app/src/main/java/com/example/test/DFhelper.java
@@ -1,54 +1,78 @@
 package com.example.test;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DFhelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
     private static final String DatabseName = "DoctorFound.db";
+    SQLiteDatabase db;
     public DFhelper(Context context) {
         super(context,DatabseName,null,VERSION);
     }
     private static final String doctorTable =
             "CREATE TABLE " + DoctorFoundSchema.DoctorTable.NAME + " (" +
-                    DoctorFoundSchema.DoctorTable.Cols.PHONE + "INTEGER PRIMARY KEY," +
-                    DoctorFoundSchema.DoctorTable.Cols.NAME + "," +
-                    DoctorFoundSchema.DoctorTable.Cols.EMAIL + " TEXT,"
-                    +       DoctorFoundSchema.DoctorTable.Cols.ADDRESS +" TEXT)" ;
+                    DoctorFoundSchema.DoctorTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
+                    DoctorFoundSchema.DoctorTable.Cols.USERNAME + " TEXT," +
+                    DoctorFoundSchema.DoctorTable.Cols.PASSWORD + " TEXT," +
+                    DoctorFoundSchema.DoctorTable.Cols.NAME + " TEXT," +
+                    DoctorFoundSchema.DoctorTable.Cols.EMAIL + " TEXT,"+
+                    DoctorFoundSchema.DoctorTable.Cols.BIRTHDATE + " TEXT," +
+                    DoctorFoundSchema.DoctorTable.Cols.ADDRESS + " TEXT,"
+                    +       DoctorFoundSchema.DoctorTable.Cols.CERTIFICATION +" TEXT)" ;
 
     private static final String patientTable =
             "CREATE TABLE " + DoctorFoundSchema.PatientTable.NAME + " (" +
                     DoctorFoundSchema.PatientTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
+                    DoctorFoundSchema.PatientTable.Cols.USERNAME + " TEXT," +
+                    DoctorFoundSchema.PatientTable.Cols.PASSWORD + " TEXT," +
                     DoctorFoundSchema.PatientTable.Cols.NAME + " TEXT," +
-                    DoctorFoundSchema.DoctorTable.Cols.EMAIL + " TEXT,"
-                    +      DoctorFoundSchema.PatientTable.Cols.ADDRESS +" TEXT)" ;
+                    DoctorFoundSchema.PatientTable.Cols.EMAIL + " TEXT,"+
+                    DoctorFoundSchema.PatientTable.Cols.BIRTHDATE + " TEXT," +
+                    DoctorFoundSchema.PatientTable.Cols.ADDRESS + " TEXT," +
+                    DoctorFoundSchema.PatientTable.Cols.CREDITNUMBER + " INTEGER," +
+                    DoctorFoundSchema.PatientTable.Cols.CVV + " INTEGER,"
+                    +      DoctorFoundSchema.PatientTable.Cols.EXPDATE +" INTEGER)" ;
+
     private static final String cashierTable =
             "CREATE TABLE " + DoctorFoundSchema.CashierTable.NAME + " (" +
                     DoctorFoundSchema.CashierTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
+                    DoctorFoundSchema.CashierTable.Cols.USERNAME + " TEXT," +
+                    DoctorFoundSchema.CashierTable.Cols.PASSWORD + " TEXT," +
                     DoctorFoundSchema.CashierTable.Cols.NAME + " TEXT," +
-                    DoctorFoundSchema.CashierTable.Cols.ADDRESS + " TEXT," +
-                    DoctorFoundSchema.CashierTable.Cols.EMAIL + " TEXT" + ")";
+                    DoctorFoundSchema.CashierTable.Cols.EMAIL + " TEXT,"+
+                    DoctorFoundSchema.CashierTable.Cols.BIRTHDATE + " TEXT," +
+                    DoctorFoundSchema.CashierTable.Cols.ADDRESS + " TEXT,"
+                    +       DoctorFoundSchema.CashierTable.Cols.CERTIFICATION +" TEXT)" ;
+
     private static final String adminTable =
             "CREATE TABLE " + DoctorFoundSchema.AdminTable.NAME + " (" +
                     DoctorFoundSchema.AdminTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
+                    DoctorFoundSchema.AdminTable.Cols.USERNAME + " TEXT," +
+                    DoctorFoundSchema.AdminTable.Cols.PASSWORD + " TEXT," +
                     DoctorFoundSchema.AdminTable.Cols.NAME + " TEXT," +
-                    DoctorFoundSchema.AdminTable.Cols.ADDRESS + " TEXT," +
-                    DoctorFoundSchema.AdminTable.Cols.EMAIL + " TEXT" + ")";
+                    DoctorFoundSchema.AdminTable.Cols.EMAIL + " TEXT,"+
+                    DoctorFoundSchema.AdminTable.Cols.BIRTHDATE + " TEXT,"
+                    +       DoctorFoundSchema.AdminTable.Cols.ADDRESS +" TEXT)" ;
+
     private static final String appointmentTable =
             "CREATE TABLE " + DoctorFoundSchema.AppointmentTable.NAME + " (" +
                     DoctorFoundSchema.AppointmentTable.Cols.APPOINTMENT_NUMBER + "INTEGER PRIMARY KEY, "+
                     DoctorFoundSchema.AppointmentTable.Cols.DATE +" TEXT,"+
                     DoctorFoundSchema.AppointmentTable.Cols.DOCTOR_NAME + " TEXT,"+
                     DoctorFoundSchema.AppointmentTable.Cols.PATIENT_NAME + " TEXT" + ")";
+
     private static final String paymentTable =
             "CREATE TABLE " + DoctorFoundSchema.PaymentTable.NAME + " (" +
                     DoctorFoundSchema.PaymentTable.Cols.PAYMENT_ID + "INTEGER PRIMARY KEY, "+
                     DoctorFoundSchema.PaymentTable.Cols.BALANCE+" INTEGER, "+
                     DoctorFoundSchema.PaymentTable.Cols.DATE+" TEXT, "+
-                    DoctorFoundSchema.PaymentTable.Cols.DOCTOR_NAME+" TEXT," +
-                    DoctorFoundSchema.PaymentTable.Cols.INSURANCE+" TEXT,"+
-                    DoctorFoundSchema.PaymentTable.Cols.PATIENT_NAME+" TEXT"+")";
+                    DoctorFoundSchema.PaymentTable.Cols.DOCTOR_NAME+" TEXT, " +
+                    DoctorFoundSchema.PaymentTable.Cols.INSURANCE+" TEXT, "+
+                    DoctorFoundSchema.PaymentTable.Cols.PATIENT_NAME+" TEXT "+")";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -65,5 +89,138 @@ public class DFhelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    //CUSTOM METHODS
+    //create seperate insert method for doctor, patient, admin, and cashier
+
+
+    //write doctor to database
+    public void insertDoctor(DoctorFoundSchema dfs){
+
+        //used to write to database
+        db = this.getWritableDatabase();
+        ContentValues valuesDoctor = new ContentValues();
+
+        String query = "SELECT * FROM " + DoctorFoundSchema.DoctorTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.PHONE, dfs.getDoctorPhone());
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.USERNAME, dfs.getDoctorUsername());
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.PASSWORD, dfs.getDoctorPassword());
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.NAME, dfs.getDoctorFullName());
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.EMAIL, dfs.getDoctorEmail());
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.BIRTHDATE, dfs.getDoctorBirthdate());
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.ADDRESS, dfs.getDoctorAddress());
+        valuesDoctor.put(DoctorFoundSchema.DoctorTable.Cols.CERTIFICATION, dfs.getDoctorCertification());
+
+        db.insert(DoctorFoundSchema.DoctorTable.NAME, null, valuesDoctor);
+        db.close(); //close at end
+        cursor.close();
+    }
+
+    //write patient to database
+    public void insertPatient(DoctorFoundSchema dfs){
+
+        //used to write to database
+        db = this.getWritableDatabase();
+        ContentValues valuesPatient = new ContentValues();
+
+        String query = "SELECT * FROM " + DoctorFoundSchema.PatientTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.PHONE, dfs.getPatientPhone());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.USERNAME, dfs.getPatientUsername());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.PASSWORD, dfs.getPatientPassword());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.NAME, dfs.getPatientFullName());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.EMAIL, dfs.getPatientEmail());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.BIRTHDATE, dfs.getPatientBirthdate());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.ADDRESS, dfs.getPatientAddress());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.CREDITNUMBER, dfs.getPatientCreditNumber());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.CVV, dfs.getPatientCVV());
+        valuesPatient.put(DoctorFoundSchema.PatientTable.Cols.EXPDATE, dfs.getPatientExpDate());
+
+        db.insert(DoctorFoundSchema.PatientTable.NAME, null, valuesPatient);
+        db.close(); //close at end
+        cursor.close();
+    }
+    //write cashier to database
+    public void insertCashier(DoctorFoundSchema dfs){
+
+        //used to write to database
+        db = this.getWritableDatabase();
+        ContentValues valuesCashier = new ContentValues();
+
+        String query = "SELECT * FROM " + DoctorFoundSchema.CashierTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.PHONE, dfs.getCashierPhone());
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.USERNAME, dfs.getCashierUsername());
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.PASSWORD, dfs.getCashierPassword());
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.NAME, dfs.getCashierFullName());
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.EMAIL, dfs.getCashierEmail());
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.BIRTHDATE, dfs.getCashierBirthdate());
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.ADDRESS, dfs.getCashierAddress());
+        valuesCashier.put(DoctorFoundSchema.CashierTable.Cols.CERTIFICATION, dfs.getCashierCertification());
+
+        db.insert(DoctorFoundSchema.CashierTable.NAME, null, valuesCashier);
+        db.close(); //close at end
+        cursor.close();
+    }
+
+    //write admin to database
+    public void insertAdmin(DoctorFoundSchema dfs){
+
+        //used to write to database
+        db = this.getWritableDatabase();
+        ContentValues valuesAdmin = new ContentValues();
+
+        String query = "SELECT * FROM " + DoctorFoundSchema.AdminTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+
+        valuesAdmin.put(DoctorFoundSchema.AdminTable.Cols.PHONE, dfs.getAdminPhone());
+        valuesAdmin.put(DoctorFoundSchema.AdminTable.Cols.USERNAME, dfs.getAdminUsername());
+        valuesAdmin.put(DoctorFoundSchema.AdminTable.Cols.PASSWORD, dfs.getAdminPassword());
+        valuesAdmin.put(DoctorFoundSchema.AdminTable.Cols.NAME, dfs.getAdminFullName());
+        valuesAdmin.put(DoctorFoundSchema.AdminTable.Cols.EMAIL, dfs.getAdminEmail());
+        valuesAdmin.put(DoctorFoundSchema.AdminTable.Cols.BIRTHDATE, dfs.getAdminBirthdate());
+        valuesAdmin.put(DoctorFoundSchema.AdminTable.Cols.ADDRESS, dfs.getAdminAddress());
+
+        db.insert(DoctorFoundSchema.AdminTable.NAME, null, valuesAdmin);
+        db.close(); //close at end
+        cursor.close();
+    }
+
+
+
+
+
+//    //open database, read, find and return password(String)
+//    public String searchPass(String uname){
+//
+//        //Read from database, using database object at top of code
+//        db = this.getReadableDatabase();
+//
+//        //create query, use cursor to use query
+//        String query = "select uname, pass from " + TABLE_NAME;
+//        Cursor cursor = db.rawQuery(query, null);
+//        String Uname, Pass = "password not found";
+//
+//        //iterate through all passwords, if find correct uname, break out of loop
+//        if(cursor.moveToFirst()){
+//            do {
+//                Uname = cursor.getString(0);
+//
+//                if(Uname.equals(uname)){          //"uname" is parameter passed into this custom method
+//                    Pass = cursor.getString(1);  //set Pass, to corresponding password
+//                    break; //break out of do-while loop
+//                }
+//            }while(cursor.moveToNext());
+//        }
+//        cursor.close();
+//        db.close();
+//        return Pass; //return this password, for comparisons
+//    }
+
+
 }
 
--- app/src/main/java/com/example/test/DoctorFoundSchema.java
+++ app/src/main/java/com/example/test/DoctorFoundSchema.java
@@ -5,11 +5,15 @@ public class DoctorFoundSchema {
     public static final class DoctorTable{
         public static final String NAME = "Doctor";
         public static final class Cols {
-            public static final String PHONE="Phone Number";
-            public static final String ADDRESS="Office Address";
-            public static final String NAME = "Full Name";
-            public static final String EMAIL="Email";
 
+            public static final String USERNAME="Username";
+            public static final String PASSWORD="Password";
+            public static final String NAME = "FullName";
+            public static final String PHONE="PhoneNumber";
+            public static final String EMAIL="Email";
+            public static final String BIRTHDATE="Birthdate";
+            public static final String ADDRESS="OfficeAddress";
+            public static final String CERTIFICATION="Certification";
 
         }
 
@@ -17,31 +21,43 @@ public class DoctorFoundSchema {
     public static final class PatientTable{
         public static final String NAME = "Patient";
         public static final class Cols {
-            public static final String PHONE="Phone Number";
-            public static final String ADDRESS="Home Address";
-            public static final String NAME = "Full Name";
+
+            public static final String USERNAME="Username";
+            public static final String PASSWORD="Password";
+            public static final String NAME = "FullName";
+            public static final String PHONE="PhoneNumber";
             public static final String EMAIL="Email";
-
-
+            public static final String BIRTHDATE="Birthdate";
+            public static final String ADDRESS="OfficeAddress";
+            public static final String CREDITNUMBER="CreditNumber";
+            public static final String CVV="Cvv";
+            public static final String EXPDATE="ExpirationDate";
         }
     }
     public static final class CashierTable{
         public static final String NAME = "Cashier";
         public static final class Cols {
-            public static final String PHONE="Phone Number";
-            public static final String ADDRESS="Home Address";
-            public static final String NAME = "Full Name";
+            public static final String USERNAME="Username";
+            public static final String PASSWORD="Password";
+            public static final String NAME = "FullName";
+            public static final String PHONE="PhoneNumber";
             public static final String EMAIL="Email";
+            public static final String BIRTHDATE="Birthdate";
+            public static final String ADDRESS="OfficeAddress";
+            public static final String CERTIFICATION="Certification";
         }
 
     }
     public static final class AdminTable{
         public static final String NAME = "Admin";
         public static final class Cols {
-            public static final String PHONE="Phone Number";
-            public static final String ADDRESS="Home Address";
-            public static final String NAME = "Full Name";
+            public static final String USERNAME="Username";
+            public static final String PASSWORD="Password";
+            public static final String NAME = "FullName";
+            public static final String PHONE="PhoneNumber";
             public static final String EMAIL="Email";
+            public static final String BIRTHDATE="Birthdate";
+            public static final String ADDRESS="OfficeAddress";
 
         }
 
@@ -50,12 +66,9 @@ public class DoctorFoundSchema {
         public static final String NAME = "Appointment";
         public static final class Cols {
             public static final String DATE = "Date";
-            public static final String PATIENT_NAME="Patient Name";
-            public static final String DOCTOR_NAME = "Doctor Name";
-            public static final String APPOINTMENT_NUMBER = "Appointment Number";
-
-
-
+            public static final String PATIENT_NAME="PatientName";
+            public static final String DOCTOR_NAME = "DoctorName";
+            public static final String APPOINTMENT_NUMBER = "AppointmentNumber";
         }
 
     }
@@ -63,12 +76,287 @@ public class DoctorFoundSchema {
         public static final String NAME = "Payment";
         public static final class Cols {
             public static final String DATE = "Date";
-            public static final String PATIENT_NAME="Patient Name";
-            public static final String DOCTOR_NAME = "Doctor Name";
+            public static final String PATIENT_NAME="PatientName";
+            public static final String DOCTOR_NAME = "DoctorName";
             public static final String INSURANCE ="Insurance";
             public static final String BALANCE = "Balance";
-            public static final String PAYMENT_ID = "Payment Id";
+            public static final String PAYMENT_ID = "PaymentId";
         }
 
+    }
+
+    //get & set methods for user login/create account
+    String patientUsername, doctorUsername, adminUsername, cashierUsername;
+    String patientPassword, doctorPassword, adminPassword, cashierPassword;
+    String patientFullName, doctorFullName, adminFullName, cashierFullName;
+    String patientPhone, doctorPhone, adminPhone, cashierPhone;
+    String patientEmail, doctorEmail, adminEmail, cashierEmail;
+    String patientBirthdate, doctorBirthdate, adminBirthdate, cashierBirthdate;
+    String patientAddress, doctorAddress, adminAddress, cashierAddress;
+    String doctorCertification, cashierCertification;
+    int patientCreditNumber, patientCVV, patientExpDate;
+
+    public int getPatientCreditNumber() {
+        return patientCreditNumber;
+    }
+
+    public void setPatientCreditNumber(int patientCreditNumber) {
+        this.patientCreditNumber = patientCreditNumber;
+    }
+
+    public int getPatientCVV() {
+        return patientCVV;
+    }
+
+    public void setPatientCVV(int patientCVV) {
+        this.patientCVV = patientCVV;
+    }
+
+    public int getPatientExpDate() {
+        return patientExpDate;
+    }
+
+    public void setPatientExpDate(int patientExpDate) {
+        this.patientExpDate = patientExpDate;
+    }
+
+    public String getPatientUsername() {
+        return patientUsername;
+    }
+
+    public void setPatientUsername(String patientUsername) {
+        this.patientUsername = patientUsername;
+    }
+
+    public String getDoctorUsername() {
+        return doctorUsername;
+    }
+
+    public void setDoctorUsername(String doctorUsername) {
+        this.doctorUsername = doctorUsername;
+    }
+
+    public String getAdminUsername() {
+        return adminUsername;
+    }
+
+    public void setAdminUsername(String adminUsername) {
+        this.adminUsername = adminUsername;
+    }
+
+    public String getCashierUsername() {
+        return cashierUsername;
+    }
+
+    public void setCashierUsername(String cashierUsername) {
+        this.cashierUsername = cashierUsername;
+    }
+
+    public String getPatientPassword() {
+        return patientPassword;
+    }
+
+    public void setPatientPassword(String patientPassword) {
+        this.patientPassword = patientPassword;
+    }
+
+    public String getDoctorPassword() {
+        return doctorPassword;
+    }
+
+    public void setDoctorPassword(String doctorPassword) {
+        this.doctorPassword = doctorPassword;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public String getCashierPassword() {
+        return cashierPassword;
+    }
+
+    public void setCashierPassword(String cashierPassword) {
+        this.cashierPassword = cashierPassword;
+    }
+
+    public String getPatientFullName() {
+        return patientFullName;
+    }
+
+    public void setPatientFullName(String patientFullName) {
+        this.patientFullName = patientFullName;
+    }
+
+    public String getDoctorFullName() {
+        return doctorFullName;
+    }
+
+    public void setDoctorFullName(String doctorFullName) {
+        this.doctorFullName = doctorFullName;
+    }
+
+    public String getAdminFullName() {
+        return adminFullName;
+    }
+
+    public void setAdminFullName(String adminFullName) {
+        this.adminFullName = adminFullName;
+    }
+
+    public String getCashierFullName() {
+        return cashierFullName;
+    }
+
+    public void setCashierFullName(String cashierFullName) {
+        this.cashierFullName = cashierFullName;
+    }
+
+    public String getPatientPhone() {
+        return patientPhone;
+    }
+
+    public void setPatientPhone(String patientPhone) {
+        this.patientPhone = patientPhone;
+    }
+
+    public String getDoctorPhone() {
+        return doctorPhone;
+    }
+
+    public void setDoctorPhone(String doctorPhone) {
+        this.doctorPhone = doctorPhone;
+    }
+
+    public String getAdminPhone() {
+        return adminPhone;
+    }
+
+    public void setAdminPhone(String adminPhone) {
+        this.adminPhone = adminPhone;
+    }
+
+    public String getCashierPhone() {
+        return cashierPhone;
+    }
+
+    public void setCashierPhone(String cashierPhone) {
+        this.cashierPhone = cashierPhone;
+    }
+
+    public String getPatientEmail() {
+        return patientEmail;
+    }
+
+    public void setPatientEmail(String patientEmail) {
+        this.patientEmail = patientEmail;
+    }
+
+    public String getDoctorEmail() {
+        return doctorEmail;
+    }
+
+    public void setDoctorEmail(String doctorEmail) {
+        this.doctorEmail = doctorEmail;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+
+    public String getCashierEmail() {
+        return cashierEmail;
+    }
+
+    public void setCashierEmail(String cashierEmail) {
+        this.cashierEmail = cashierEmail;
+    }
+
+    public String getPatientBirthdate() {
+        return patientBirthdate;
+    }
+
+    public void setPatientBirthdate(String patientBirthdate) {
+        this.patientBirthdate = patientBirthdate;
+    }
+
+    public String getDoctorBirthdate() {
+        return doctorBirthdate;
+    }
+
+    public void setDoctorBirthdate(String doctorBirthdate) {
+        this.doctorBirthdate = doctorBirthdate;
+    }
+
+    public String getAdminBirthdate() {
+        return adminBirthdate;
+    }
+
+    public void setAdminBirthdate(String adminBirthdate) {
+        this.adminBirthdate = adminBirthdate;
+    }
+
+    public String getCashierBirthdate() {
+        return cashierBirthdate;
+    }
+
+    public void setCashierBirthdate(String cashierBirthdate) {
+        this.cashierBirthdate = cashierBirthdate;
+    }
+
+    public String getPatientAddress() {
+        return patientAddress;
+    }
+
+    public void setPatientAddress(String patientAddress) {
+        this.patientAddress = patientAddress;
+    }
+
+    public String getDoctorAddress() {
+        return doctorAddress;
+    }
+
+    public void setDoctorAddress(String doctorAddress) {
+        this.doctorAddress = doctorAddress;
+    }
+
+    public String getAdminAddress() {
+        return adminAddress;
+    }
+
+    public void setAdminAddress(String adminAddress) {
+        this.adminAddress = adminAddress;
+    }
+
+    public String getCashierAddress() {
+        return cashierAddress;
+    }
+
+    public void setCashierAddress(String cashierAddress) {
+        this.cashierAddress = cashierAddress;
+    }
+
+    public String getDoctorCertification() {
+        return doctorCertification;
+    }
+
+    public void setDoctorCertification(String doctorCertification) {
+        this.doctorCertification = doctorCertification;
+    }
+
+    public String getCashierCertification() {
+        return cashierCertification;
+    }
+
+    public void setCashierCertification(String cashierCertification) {
+        this.cashierCertification = cashierCertification;
     }
 }
