5c170523efd73df6c807ee252953de0d1f85e130
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-04-04 04:08:06 -0700

    Added more DFhelper code. And changed some strings and layout to suit tables. Also added spaces in database table creation, where needed.

--- app/src/main/java/com/example/test/DFhelper.java
+++ app/src/main/java/com/example/test/DFhelper.java
@@ -13,56 +13,57 @@ public class DFhelper extends SQLiteOpenHelper {
     public DFhelper(Context context) {
         super(context,DatabseName,null,VERSION);
     }
+
     private static final String doctorTable =
             "CREATE TABLE " + DoctorFoundSchema.DoctorTable.NAME + " (" +
                     DoctorFoundSchema.DoctorTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
-                    DoctorFoundSchema.DoctorTable.Cols.USERNAME + " TEXT," +
-                    DoctorFoundSchema.DoctorTable.Cols.PASSWORD + " TEXT," +
-                    DoctorFoundSchema.DoctorTable.Cols.NAME + " TEXT," +
-                    DoctorFoundSchema.DoctorTable.Cols.EMAIL + " TEXT,"+
-                    DoctorFoundSchema.DoctorTable.Cols.BIRTHDATE + " TEXT," +
-                    DoctorFoundSchema.DoctorTable.Cols.ADDRESS + " TEXT,"
+                    DoctorFoundSchema.DoctorTable.Cols.USERNAME + " TEXT, " +
+                    DoctorFoundSchema.DoctorTable.Cols.PASSWORD + " TEXT, " +
+                    DoctorFoundSchema.DoctorTable.Cols.NAME + " TEXT, " +
+                    DoctorFoundSchema.DoctorTable.Cols.EMAIL + " TEXT, "+
+                    DoctorFoundSchema.DoctorTable.Cols.BIRTHDATE + " TEXT, " +
+                    DoctorFoundSchema.DoctorTable.Cols.ADDRESS + " TEXT, "
                     +       DoctorFoundSchema.DoctorTable.Cols.CERTIFICATION +" TEXT)" ;
 
     private static final String patientTable =
             "CREATE TABLE " + DoctorFoundSchema.PatientTable.NAME + " (" +
                     DoctorFoundSchema.PatientTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
-                    DoctorFoundSchema.PatientTable.Cols.USERNAME + " TEXT," +
-                    DoctorFoundSchema.PatientTable.Cols.PASSWORD + " TEXT," +
-                    DoctorFoundSchema.PatientTable.Cols.NAME + " TEXT," +
-                    DoctorFoundSchema.PatientTable.Cols.EMAIL + " TEXT,"+
-                    DoctorFoundSchema.PatientTable.Cols.BIRTHDATE + " TEXT," +
-                    DoctorFoundSchema.PatientTable.Cols.ADDRESS + " TEXT," +
-                    DoctorFoundSchema.PatientTable.Cols.CREDITNUMBER + " INTEGER," +
-                    DoctorFoundSchema.PatientTable.Cols.CVV + " INTEGER,"
+                    DoctorFoundSchema.PatientTable.Cols.USERNAME + " TEXT, " +
+                    DoctorFoundSchema.PatientTable.Cols.PASSWORD + " TEXT, " +
+                    DoctorFoundSchema.PatientTable.Cols.NAME + " TEXT, " +
+                    DoctorFoundSchema.PatientTable.Cols.EMAIL + " TEXT, "+
+                    DoctorFoundSchema.PatientTable.Cols.BIRTHDATE + " TEXT, " +
+                    DoctorFoundSchema.PatientTable.Cols.ADDRESS + " TEXT, " +
+                    DoctorFoundSchema.PatientTable.Cols.CREDITNUMBER + " INTEGER, " +
+                    DoctorFoundSchema.PatientTable.Cols.CVV + " INTEGER, "
                     +      DoctorFoundSchema.PatientTable.Cols.EXPDATE +" INTEGER)" ;
 
     private static final String cashierTable =
             "CREATE TABLE " + DoctorFoundSchema.CashierTable.NAME + " (" +
                     DoctorFoundSchema.CashierTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
-                    DoctorFoundSchema.CashierTable.Cols.USERNAME + " TEXT," +
-                    DoctorFoundSchema.CashierTable.Cols.PASSWORD + " TEXT," +
-                    DoctorFoundSchema.CashierTable.Cols.NAME + " TEXT," +
-                    DoctorFoundSchema.CashierTable.Cols.EMAIL + " TEXT,"+
-                    DoctorFoundSchema.CashierTable.Cols.BIRTHDATE + " TEXT," +
-                    DoctorFoundSchema.CashierTable.Cols.ADDRESS + " TEXT,"
+                    DoctorFoundSchema.CashierTable.Cols.USERNAME + " TEXT, " +
+                    DoctorFoundSchema.CashierTable.Cols.PASSWORD + " TEXT, " +
+                    DoctorFoundSchema.CashierTable.Cols.NAME + " TEXT, " +
+                    DoctorFoundSchema.CashierTable.Cols.EMAIL + " TEXT, "+
+                    DoctorFoundSchema.CashierTable.Cols.BIRTHDATE + " TEXT, " +
+                    DoctorFoundSchema.CashierTable.Cols.ADDRESS + " TEXT, "
                     +       DoctorFoundSchema.CashierTable.Cols.CERTIFICATION +" TEXT)" ;
 
     private static final String adminTable =
             "CREATE TABLE " + DoctorFoundSchema.AdminTable.NAME + " (" +
                     DoctorFoundSchema.AdminTable.Cols.PHONE + "INTEGER PRIMARY KEY, " +
-                    DoctorFoundSchema.AdminTable.Cols.USERNAME + " TEXT," +
-                    DoctorFoundSchema.AdminTable.Cols.PASSWORD + " TEXT," +
-                    DoctorFoundSchema.AdminTable.Cols.NAME + " TEXT," +
-                    DoctorFoundSchema.AdminTable.Cols.EMAIL + " TEXT,"+
-                    DoctorFoundSchema.AdminTable.Cols.BIRTHDATE + " TEXT,"
+                    DoctorFoundSchema.AdminTable.Cols.USERNAME + " TEXT, " +
+                    DoctorFoundSchema.AdminTable.Cols.PASSWORD + " TEXT, " +
+                    DoctorFoundSchema.AdminTable.Cols.NAME + " TEXT, " +
+                    DoctorFoundSchema.AdminTable.Cols.EMAIL + " TEXT, "+
+                    DoctorFoundSchema.AdminTable.Cols.BIRTHDATE + " TEXT, "
                     +       DoctorFoundSchema.AdminTable.Cols.ADDRESS +" TEXT)" ;
 
     private static final String appointmentTable =
             "CREATE TABLE " + DoctorFoundSchema.AppointmentTable.NAME + " (" +
                     DoctorFoundSchema.AppointmentTable.Cols.APPOINTMENT_NUMBER + "INTEGER PRIMARY KEY, "+
-                    DoctorFoundSchema.AppointmentTable.Cols.DATE +" TEXT,"+
-                    DoctorFoundSchema.AppointmentTable.Cols.DOCTOR_NAME + " TEXT,"+
+                    DoctorFoundSchema.AppointmentTable.Cols.DATE +" TEXT, "+
+                    DoctorFoundSchema.AppointmentTable.Cols.DOCTOR_NAME + " TEXT, "+
                     DoctorFoundSchema.AppointmentTable.Cols.PATIENT_NAME + " TEXT" + ")";
 
     private static final String paymentTable =
@@ -80,7 +81,7 @@ public class DFhelper extends SQLiteOpenHelper {
         db.execSQL(patientTable);
         db.execSQL(cashierTable);
         db.execSQL(adminTable);
-        db.execSQL(cashierTable);
+        db.execSQL(appointmentTable);
         db.execSQL(paymentTable);
 
     }
@@ -91,9 +92,8 @@ public class DFhelper extends SQLiteOpenHelper {
     }
 
     //CUSTOM METHODS
+
     //create seperate insert method for doctor, patient, admin, and cashier
-
-
     //write doctor to database
     public void insertDoctor(DoctorFoundSchema dfs){
 
@@ -117,7 +117,6 @@ public class DFhelper extends SQLiteOpenHelper {
         db.close(); //close at end
         cursor.close();
     }
-
     //write patient to database
     public void insertPatient(DoctorFoundSchema dfs){
 
@@ -166,7 +165,6 @@ public class DFhelper extends SQLiteOpenHelper {
         db.close(); //close at end
         cursor.close();
     }
-
     //write admin to database
     public void insertAdmin(DoctorFoundSchema dfs){
 
@@ -190,36 +188,121 @@ public class DFhelper extends SQLiteOpenHelper {
         cursor.close();
     }
 
+    //Search Password for Each User type- open database, read, find and return password(String)
+    //Doctor
+    public String searchDoctorPass(String doctorUsername){
+
+        //Read from database, using database object at top of code
+        db = this.getReadableDatabase();
+
+        //create query, use cursor to use query
+        String query = "SELECT " +DoctorFoundSchema.DoctorTable.Cols.USERNAME +", " +
+                DoctorFoundSchema.DoctorTable.Cols.PASSWORD + " FROM " + DoctorFoundSchema.DoctorTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        String Uname;
+        String pass = "password not found";
+
+        //iterate through all passwords, if find correct uname, break out of loop
+        if(cursor.moveToFirst()){
+            do {
+                Uname = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.DoctorTable.Cols.USERNAME));
+
+                if(Uname.equals(doctorUsername)){
+                    pass = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.DoctorTable.Cols.PASSWORD));  //set Pass, to corresponding password
+                    break; //break out of do-while loop
+                }
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return pass; //return this password, for comparisons
+    }
+    //Patient
+    public String searchPatientPass(String patientUsername){
+
+        //Read from database, using database object at top of code
+        db = this.getReadableDatabase();
+
+        //create query, use cursor to use query
+        String query = "SELECT " +DoctorFoundSchema.PatientTable.Cols.USERNAME +", " +
+                DoctorFoundSchema.PatientTable.Cols.PASSWORD + " FROM " + DoctorFoundSchema.PatientTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        String Uname;
+        String pass = "password not found";
+
+        //iterate through all passwords, if find correct uname, break out of loop
+        if(cursor.moveToFirst()){
+            do {
+                Uname = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.PatientTable.Cols.USERNAME));
+
+                if(Uname.equals(patientUsername)){
+                    pass = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.PatientTable.Cols.PASSWORD));  //set Pass, to corresponding password
+                    break; //break out of do-while loop
+                }
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return pass; //return this password, for comparisons
+    }
+    //Cashier
+    public String searchCashierPass(String cashierUsername){
+
+        //Read from database, using database object at top of code
+        db = this.getReadableDatabase();
+
+        //create query, use cursor to use query
+        String query = "SELECT " +DoctorFoundSchema.CashierTable.Cols.USERNAME +", " +
+                DoctorFoundSchema.CashierTable.Cols.PASSWORD + " FROM " + DoctorFoundSchema.CashierTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        String Uname;
+        String pass = "password not found";
+
+        //iterate through all passwords, if find correct uname, break out of loop
+        if(cursor.moveToFirst()){
+            do {
+                Uname = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.CashierTable.Cols.USERNAME));
+
+                if(Uname.equals(cashierUsername)){
+                    pass = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.CashierTable.Cols.PASSWORD));  //set Pass, to corresponding password
+                    break; //break out of do-while loop
+                }
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return pass; //return this password, for comparisons
+    }
+    //Admin
+    public String searchAdminPass(String adminUsername){
+
+        //Read from database, using database object at top of code
+        db = this.getReadableDatabase();
+
+        //create query, use cursor to use query
+        String query = "SELECT " +DoctorFoundSchema.AdminTable.Cols.USERNAME +", " +
+                DoctorFoundSchema.AdminTable.Cols.PASSWORD + " FROM " + DoctorFoundSchema.AdminTable.NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        String Uname;
+        String pass = "password not found";
+
+        //iterate through all passwords, if find correct uname, break out of loop
+        if(cursor.moveToFirst()){
+            do {
+                Uname = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.AdminTable.Cols.USERNAME));
+
+                if(Uname.equals(adminUsername)){
+                    pass = cursor.getString(cursor.getColumnIndex(DoctorFoundSchema.AdminTable.Cols.PASSWORD));  //set Pass, to corresponding password
+                    break; //break out of do-while loop
+                }
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return pass; //return this password, for comparisons
+    }
 
 
-
-
-//    //open database, read, find and return password(String)
-//    public String searchPass(String uname){
-//
-//        //Read from database, using database object at top of code
-//        db = this.getReadableDatabase();
-//
-//        //create query, use cursor to use query
-//        String query = "select uname, pass from " + TABLE_NAME;
-//        Cursor cursor = db.rawQuery(query, null);
-//        String Uname, Pass = "password not found";
-//
-//        //iterate through all passwords, if find correct uname, break out of loop
-//        if(cursor.moveToFirst()){
-//            do {
-//                Uname = cursor.getString(0);
-//
-//                if(Uname.equals(uname)){          //"uname" is parameter passed into this custom method
-//                    Pass = cursor.getString(1);  //set Pass, to corresponding password
-//                    break; //break out of do-while loop
-//                }
-//            }while(cursor.moveToNext());
-//        }
-//        cursor.close();
-//        db.close();
-//        return Pass; //return this password, for comparisons
-//    }
 
 
 }
