667536747243509daf5e30b6345c5579a8e4b364
JoeyMai99 <mait4@student.douglascollege.ca>
2020-04-04 23:36:30 -0700

    push

---
+++ app/src/main/java/com/bignerdranch/android/dfdatabase/database/DoctorFoundSchema.java
@@ -0,0 +1,73 @@
+package com.bignerdranch.android.dfdatabase.database;
+
+public class DoctorFoundSchema {
+    public static final class DoctorTable{
+        public static final String NAME = "Doctor";
+        public static final class Cols {
+            public static final String PHONE="Phone Number";
+            public static final String ADDRESS="Office Address";
+            public static final String NAME = "Full Name";
+            public static final String EMAIL="Email";
+
+
+        }
+
+    }
+    public static final class PatientTable{
+        public static final String NAME = "Patient";
+        public static final class Cols {
+            public static final String PHONE="Phone Number";
+            public static final String ADDRESS="Home Address";
+            public static final String NAME = "Full Name";
+            public static final String EMAIL="Email";
+
+
+        }
+    }
+    public static final class CashierTable{
+        public static final String NAME = "Cashier";
+        public static final class Cols {
+            public static final String PHONE="Phone Number";
+            public static final String ADDRESS="Home Address";
+            public static final String NAME = "Full Name";
+            public static final String EMAIL="Email";
+        }
+
+    }
+    public static final class AdminTable{
+        public static final String NAME = "Admin";
+        public static final class Cols {
+            public static final String PHONE="Phone Number";
+            public static final String ADDRESS="Home Address";
+            public static final String NAME = "Full Name";
+            public static final String EMAIL="Email";
+
+        }
+
+    }
+    public static final class AppointmentTable{
+        public static final String NAME = "Appointment";
+        public static final class Cols {
+            public static final String DATE = "Date";
+            public static final String PATIENT_NAME="Patient Name";
+            public static final String DOCTOR_NAME = "Doctor Name";
+            public static final String APPOINTMENT_NUMBER = "Appointment Number";
+
+
+
+        }
+
+    }
+    public static final class PaymentTable{
+        public static final String NAME = "Payment";
+        public static final class Cols {
+            public static final String DATE = "Date";
+            public static final String PATIENT_NAME="Patient Name";
+            public static final String DOCTOR_NAME = "Doctor Name";
+            public static final String INSURANCE ="Insurance";
+            public static final String BALANCE = "Balance";
+            public static final String PAYMENT_ID = "Payment Id";
+        }
+
+    }
+}
