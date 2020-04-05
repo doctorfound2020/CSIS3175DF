43159bf439e55ebec3466059f695e549ea95fcfb
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-04-05 00:45:14 -0700

    Completed the last code required for AdminHomePageActivity.java. Should be no furthur changes made to code from here...........

--- app/src/main/java/com/example/test/AdminHomePageActivity.java
+++ app/src/main/java/com/example/test/AdminHomePageActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class AdminHomePageActivity extends AppCompatActivity {
 
@@ -25,52 +26,62 @@ public class AdminHomePageActivity extends AppCompatActivity {
         Button btnSignOutAdmin = findViewById(R.id.btnSignOutAdmin);
         Button btnEditProfileAdmin = findViewById(R.id.btnEditProfileAdmin);
 
+        final Spinner sChooseUserSignUpEdit = findViewById(R.id.sChooseUserSignUpEdit); //must change value of this depending on button clicked
+
 
         btnAddPatient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(0);
                 startActivity(new Intent(AdminHomePageActivity.this, SignUpActivity.class));
             }
         });
         btnEditPatient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(0);
                 startActivity(new Intent(AdminHomePageActivity.this, EditAccountActivity.class));
             }
         });
         btnAddDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(1);
                 startActivity(new Intent(AdminHomePageActivity.this, SignUpActivity.class));
             }
         });
         btnEditDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(1);
                 startActivity(new Intent(AdminHomePageActivity.this, EditAccountActivity.class));
             }
         });
         btnAddCashier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(2);
                 startActivity(new Intent(AdminHomePageActivity.this, SignUpActivity.class));
             }
         });
         btnEditCashier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(2);
                 startActivity(new Intent(AdminHomePageActivity.this, EditAccountActivity.class));
             }
         });
         btnSignOutAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(3);
                 startActivity(new Intent(AdminHomePageActivity.this, LogoutActivity.class));
             }
         });
         btnEditProfileAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sChooseUserSignUpEdit.setSelection(3);
                 startActivity(new Intent(AdminHomePageActivity.this, EditAccountActivity.class));
             }
         });
